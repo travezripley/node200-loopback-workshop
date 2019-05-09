ec5b733f7bc1b7a6891fafbd5e30f7fa436a198d
Travis Ripley <theripleys@gmail.com>
2019-05-09 16:44:43 -0700

    adds initial project files

---
+++ server/datasources.local.js
@@ -0,0 +1,22 @@
+"use strict";
+module.exports = {
+  db: {
+    name: "db",
+    connector: "memory"
+  },
+  MongoDB: {
+    url: "mongodb://localhost:27017",
+    name: "MongoDB",
+    connector: "mongodb"
+  },
+"MongoDB": {
+  "host": "",
+  "port": 0,
+  "url": "",
+  "database": "",
+  "password": "",
+  "name": "MongoDB",
+  "user": "",
+  "connector": "mongodb"
+}
+};
